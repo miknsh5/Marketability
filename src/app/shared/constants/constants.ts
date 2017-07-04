29cd2a91f73d6571f0d05cc55a1fb59540ed3a48
Moorthy <moorthy.yadav@cennest.com>
2017-07-04 17:01:08 +0530

    Score Page CSS changes

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -2,8 +2,6 @@ export class Constants {
         public static PageTitles=[{Id:0, Name:'Profile'},
                                   {Id:1, Name:'Skills'},
                                   {Id:2, Name:'Experience'},
-                                  {Id:3, Name:'Computation'},
+                                  {Id:3, Name:'Calculating'},
                                   {Id:4, Name:'Marketability'}];
-    /*public static PageTitles= ['Profile', 'Skills', 'Experience',
-        'Computation', 'Marketability']*/
 }
